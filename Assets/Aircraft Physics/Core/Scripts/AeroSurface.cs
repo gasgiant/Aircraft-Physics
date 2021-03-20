214d1f303b5d679afa69934be4749996b277252d
Ivan Pensionerov <31007222+gasgiant@users.noreply.github.com>
2021-03-20 12:42:38 +0300

    Fixed bug in FlapEffectivenessCorrection

--- Assets/Aircraft Physics/Core/Scripts/AeroSurface.cs
+++ Assets/Aircraft Physics/Core/Scripts/AeroSurface.cs
@@ -89,10 +89,10 @@ public class AeroSurface : MonoBehaviour
         Vector3 aerodynamicCoefficients;
 
         // Low angles of attack mode and stall mode curves are stitched together by a line segment. 
-        float paddingAnglHigh = Mathf.Deg2Rad * Mathf.Lerp(15, 5, (Mathf.Rad2Deg * flapAngle + 50) / 100);
-        float paddingAnglLow = Mathf.Deg2Rad * Mathf.Lerp(15, 5, (-Mathf.Rad2Deg * flapAngle + 50) / 100);
-        float paddedStallAngleHigh = stallAngleHigh + paddingAnglHigh;
-        float paddedStallAngleLow = stallAngleLow - paddingAnglLow;
+        float paddingAngleHigh = Mathf.Deg2Rad * Mathf.Lerp(15, 5, (Mathf.Rad2Deg * flapAngle + 50) / 100);
+        float paddingAngleLow = Mathf.Deg2Rad * Mathf.Lerp(15, 5, (-Mathf.Rad2Deg * flapAngle + 50) / 100);
+        float paddedStallAngleHigh = stallAngleHigh + paddingAngleHigh;
+        float paddedStallAngleLow = stallAngleLow - paddingAngleLow;
 
         if (angleOfAttack < stallAngleHigh && angleOfAttack > stallAngleLow)
         {
@@ -183,7 +183,7 @@ public class AeroSurface : MonoBehaviour
         inducedAngle = Mathf.Lerp(0, inducedAngle, lerpParam);
         float effectiveAngle = angleOfAttack - zeroLiftAoA - inducedAngle;
 
-        float normalCoefficient = FrictionAt90Degrees() * Mathf.Sin(effectiveAngle) *
+        float normalCoefficient = FrictionAt90Degrees(flapAngle) * Mathf.Sin(effectiveAngle) *
             (1 / (0.56f + 0.44f * Mathf.Abs(Mathf.Sin(effectiveAngle))) -
             0.41f * (1 - Mathf.Exp(-17 / config.aspectRatio)));
         float tangentialCoefficient = 0.5f * config.skinFriction * Mathf.Cos(effectiveAngle);
@@ -200,14 +200,14 @@ public class AeroSurface : MonoBehaviour
         return 0.25f - 0.175f * (1 - 2 * Mathf.Abs(effectiveAngle) / Mathf.PI);
     }
 
-    private float FrictionAt90Degrees()
+    private float FrictionAt90Degrees(float flapAngle)
     {
         return 1.98f - 4.26e-2f * flapAngle * flapAngle + 2.1e-1f * flapAngle;
     }
 
     private float FlapEffectivnessCorrection(float flapAngle)
     {
-        return Mathf.Lerp(0.8f, 0.4f, (flapAngle * Mathf.Rad2Deg - 10) / 50);
+        return Mathf.Lerp(0.8f, 0.4f, (Mathf.Abs(flapAngle) * Mathf.Rad2Deg - 10) / 50);
     }
 
     private float LiftCoefficientMaxFraction(float flapFraction)
